906d6121ec18afee5bb9b17127b81717cd83c7d1
Kevin M. Smith <ksmith.nop@gmail.com>
2017-01-21 21:37:30 +0000

    Added command line options and lesson picking.

--- trainer.py
+++ trainer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import curses, os, locale, unicodedata
+import curses, os, sys, getopt
+import locale, unicodedata, json
 from curses import wrapper
 import db
 
@@ -10,6 +11,31 @@ os.environ["TERM"] = "vt220"
 locale.setlocale(locale.LC_ALL, '')
 
 db = db.DB('data.db')
+
+def json_load_byteified(file_handle):
+	return _byteify(
+		json.load(file_handle, object_hook=_byteify),
+		ignore_dicts=True
+	)
+
+def json_loads_byteified(json_text):
+	return _byteify(
+		json.loads(json_text, object_hook=_byteify),
+		ignore_dicts=True
+	)
+
+def _byteify(data, ignore_dicts = False):
+	if isinstance(data, unicode):
+		return data.encode('utf-8')
+	if isinstance(data, list):
+		return [ _byteify(item, ignore_dicts=True) for item in data ]
+	if isinstance(data, dict) and not ignore_dicts:
+		return {
+			_byteify(key, ignore_dicts=True): _byteify(value, ignore_dicts=True)
+			for key, value in data.iteritems()
+		}
+	return data
+
 
 def getAllLessons():
 	return {'lessons': db.query('SELECT ROWID, * FROM lesson')}
@@ -53,8 +79,7 @@ def drawCard(win, card, showAll = False):
 	t = len(card['side1']) * 0.75 * 1000
 	win.timeout(int(t))
 
-def main(win):
-	lessonIDs = [1]
+def run(win, lessonIDs):
 	cards = getCardsInLessons(lessonIDs)['cards']
 	i = Counter(0, 0, len(cards)-1)
 	card = cards[i.get()]
@@ -83,4 +108,45 @@ def main(win):
 		card = cards[i.get()]
 		drawCard(win, card, showAll)
 
-wrapper(main)
+options = [ 
+    ('h','help','This help.'),
+    ('t:','train=','Training mode.'),
+    ('l','list','List of all lessons.')]
+shortOpt = "".join([opt[0] for opt in options])
+longOpt = [opt[1] for opt in options]
+
+def usage():
+    pad = str(len(max(longOpt, key=len)))
+    fmt = '  -%s, --%-'+pad+'s : %s'
+    print('Usage: '+sys.argv[0]+' [options]\n')
+    for opt in options:
+        print(fmt%(opt[0][0], opt[1], opt[2]))
+    sys.exit(2)
+
+def main():
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], shortOpt, longOpt)
+	except getopt.GetoptError as err:
+		print(str(err))
+		usage()
+
+	lessonIDs = []
+	doList = False
+	for o, a in opts:
+		if o in ('-h', '--help'):
+			usage()
+		elif o in ('-t', '--train'):
+			lessonIDs.append(int(a))
+		elif o in ('-l', '--list'):
+			doList = True
+		else:
+			assert False, 'Unhandled option: ' + str(o)
+	
+	if doList:
+		print json.dumps(getAllLessons(),
+			sort_keys=True, indent=4, separators=(',', ': '))
+	else:
+		wrapper(run, lessonIDs)
+
+if __name__ == "__main__":
+	main()
